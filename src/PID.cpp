c393bb71ee41146f3042e707ce5004a0cf5a3c7f
Jeremy Shannon <jeremyeshannon@gmail.com>
2017-05-22 21:19:33 -0500

    feat: first crack - P only

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,19 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    PID::Kp = Kp;
+    PID::Ki = Ki;
+    PID::Kd = Kd;
+    p_error = d_error = i_error = 0.0;
 }
 
 void PID::UpdateError(double cte) {
+    d_error = p_error - cte;
+    p_error = cte;
+    i_error += cte;
 }
 
 double PID::TotalError() {
+    return i_error;
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -34,6 +34,7 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
+  pid.Init(1.0, 1.0, 1.0);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -57,6 +58,8 @@ int main()
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
+          pid.UpdateError(cte);
+          steer_value = -pid.Kp * cte;
           
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
